bca7485ccf6aa56518ce969ef745da04e075b9a3
Irnovi <huiralb@gmail.com>
2025-05-03 11:33:44 +0700

    init with light mode

---
+++ resources/js/composables/useAppearance.ts
@@ -0,0 +1,80 @@
+import { onMounted, ref } from 'vue';
+
+type Appearance = 'light' | 'dark' | 'system';
+
+export function updateTheme(value: Appearance) {
+    if (typeof window === 'undefined') {
+        return;
+    }
+
+    if (value === 'system') {
+        const mediaQueryList = window.matchMedia('(prefers-color-scheme: dark)');
+        const systemTheme = mediaQueryList.matches ? 'dark' : 'light';
+
+        document.documentElement.classList.toggle('dark', systemTheme === 'dark');
+    } else {
+        document.documentElement.classList.toggle('dark', value === 'dark');
+    }
+}
+
+const setCookie = (name: string, value: string, days = 365) => {
+    if (typeof document === 'undefined') {
+        return;
+    }
+
+    const maxAge = days * 24 * 60 * 60;
+
+    document.cookie = `${name}=${value};path=/;max-age=${maxAge};SameSite=Lax`;
+};
+
+const mediaQuery = () => {
+    if (typeof window === 'undefined') {
+        return null;
+    }
+
+    return window.matchMedia('(prefers-color-scheme: dark)');
+};
+
+const getStoredAppearance = () => {
+    if (typeof window === 'undefined') {
+        return null;
+    }
+
+    return localStorage.getItem('appearance') as Appearance | null;
+};
+
+const handleSystemThemeChange = () => {
+    const currentAppearance = getStoredAppearance();
+
+    updateTheme(currentAppearance || 'system');
+};
+
+export function initializeTheme() {
+    if (typeof window === 'undefined') {
+        return;
+    }
+
+    const appearance = 'light';
+    updateTheme(appearance);
+
+    // Set up system theme change listener...
+    mediaQuery()?.addEventListener('change', handleSystemThemeChange);
+}
+
+export function useAppearance() {
+    const appearance = ref<Appearance>('light');
+
+    const updateAppearance = (value: Appearance) => {
+        appearance.value = value;
+        updateTheme(value);
+    };
+
+    onMounted(() => {
+        updateTheme(appearance.value);
+    });
+
+    return {
+        appearance,
+        updateAppearance,
+    };
+}
